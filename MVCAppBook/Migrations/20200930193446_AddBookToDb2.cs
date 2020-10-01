a502a6fd8646d6e87395e2fab13b331378e8f8d3
Pushkar Ambole <pushkarambole@gmail.com>
2020-10-01 10:11:11 -0400

    Added content

---
+++ MVCAppBook/Migrations/20200930193446_AddBookToDb2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MVCAppBook.Migrations
+{
+    public partial class AddBookToDb2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
